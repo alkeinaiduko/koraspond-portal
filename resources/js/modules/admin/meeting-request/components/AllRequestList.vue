4254b9962522638299d6fd3e48cb5d254acef810
trdperaltajim <trd.peraltajim@gmail.com>
2020-02-26 15:13:19 +0800

    design madmin meeting request

---
+++ resources/js/modules/admin/meeting-request/components/AllRequestList.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <ul class="content-list">
+            <li
+                v-for="(project, key) in projects"
+                :key="key"
+            >
+                <card-container>
+                    <all-request-item :data="project" />
+                </card-container>
+            </li>
+        </ul>
+        <div class="meeting__tabs--footer--paginate">
+            <el-pagination
+                background
+                layout="prev, pager, next"
+                :total="1000"
+            />
+        </div>
+    </div>
+</template>
+<script>
+import CardContainer from '~/common/CardContainer'
+import AllRequestItem from './AllRequestItem'
+export default {
+    components: {
+        CardContainer,
+        AllRequestItem
+    },
+    data(){
+        return {
+            projects: [
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Rejected',
+                        views: 177
+                    },
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Pending',
+                        views: 177
+                    },
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Accepted',
+                        views: 177
+                    },
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Rejected',
+                        views: 177
+                    },
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Pending',
+                        views: 177
+                    },
+                    {
+                        title: 'There are many variations of passages',
+                        industry: 'Artificial Intelligence',
+                        date_submitted: '17/10/2019',
+                        status: 'Accepted',
+                        views: 177
+                    },
+                ]
+        }
+    }
+}
+</script>
---
+++ resources/views/admins/meetingrequest/index.blade.php
@@ -0,0 +1,7 @@
+  
+@extends('layouts.admin', ['vueModulePath' => 'js/modules/meetingrequest/main.js'])
+@section('page_title', 'Meeting Request')
+
+@section('content')
+    <meeting-request />
+@stop
