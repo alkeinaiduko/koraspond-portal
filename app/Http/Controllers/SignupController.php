4c6ed807a51bd5ae11566081945a0e2bb8f9365b
anullihate <lclarkdwain@outlook.com>
2020-02-26 12:35:37 +0800

    warren impl

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -11,6 +11,32 @@ use Illuminate\Support\Facades\Validator;
 
 class SignupController extends Controller
 {
+    /**
+     * Display detailed signup
+     *
+     * @return View
+     */
+    public function index()
+    {
+        //
+        if (Auth::check()) {
+            // Get the currently authenticated user
+            $user = Auth::user();
+
+            // check if registration is completed
+            if (!$user->drafted) {
+                // redirect
+                return redirect('/');
+            }
+
+            // view
+            return view('signup.detail', compact('user'));
+        } else {
+
+            // redirect
+            return redirect('/login');
+        }
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -44,7 +70,7 @@ class SignupController extends Controller
             'last_name' => $data['last_name'],
             'user_type_id' => $data['user_type_id'],
             'email' => $data['email'],
-            'password' => Hash ::make($data['password']),
+            'password' => Hash::make($data['password']),
             'country' => $data['country'],
             'city' => $data['city'],
             'address' => $data['address'],
@@ -56,7 +82,7 @@ class SignupController extends Controller
         $credentials = $request->only('email', 'password');
 
         // login user
-        if(!Auth::attempt($credentials)) {
+        if (!Auth::attempt($credentials)) {
             return response()->json(['message' => 'User created but failed to login.']);
         }
 
@@ -66,5 +92,58 @@ class SignupController extends Controller
         $user->businessRegistration()->create();
 
         return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param int $id User ID
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find(1);
+
+        // prepare arrays
+        $basicInfo = [];
+        $arrivalDetail = [];
+        $businessRegistration = [];
+
+        // table columns
+        $basicInfoColumns = array_keys($user->basicInfo->getAttributes());
+        $arrivalDetailColumns = array_keys($user->arrivalDetail->getAttributes());
+        $businessRegistrationColumns = array_keys($user->businessRegistration->getAttributes());
+
+        // basic info
+        foreach ($request->input('basic_info') as $key => $info) {
+            if (in_array($key, $basicInfoColumns))
+                $basicInfo[$key] = $info;
+        }
+
+        // arrival detail
+        foreach ($request->input('arrival_detail') as $key => $detail) {
+            if (in_array($key, $arrivalDetailColumns))
+                $arrivalDetail[$key] = $detail;
+        }
+
+        // registration detail
+        foreach ($request->input('business_registration') as $key => $reg) {
+            if (in_array($key, $businessRegistrationColumns))
+                $businessRegistration[$key] = $reg;
+        }
+
+        // update
+        $user->basicInfo()->update($basicInfo);
+        $user->arrivalDetail()->update($arrivalDetail);
+        $user->businessRegistration()->update($businessRegistration);
+
+        $user->drafted = $request->input('drafted'); // true or false, false == completed
+        $user->drafted_step = $request->input('drafted_step'); // 1 - basic info, 2 - arrival detail, 3 - b2b/b2g, 4 - b2b/b2g signup
+        $user->save();
+
+        // you can add redirection here after completed or drafted
+
+        return new UserResource($user->refresh());
     }
 }
